1107da38d5e828f7bdf087349b7c05bc5aabed1d
Kazumasa Utashiro <kaz@utashiro.com>
2021-08-29 16:52:45 +0900

    Copy test code from Text::VisualPrintf.

---
+++ t/lib/Text/VPrintf.pm
@@ -0,0 +1,57 @@
+package Text::VPrintf;
+
+use v5.14;
+use warnings;
+use Carp;
+
+use Exporter 'import';
+our @EXPORT_OK = qw(&vprintf &vsprintf);
+
+use Data::Dumper;
+use Text::Conceal;
+
+sub vprintf  { &printf (@_) }
+sub vsprintf { &sprintf(@_) }
+
+our $IS_TARGET = qr/[\e\b\P{ASCII}]/;
+our $VISUAL_WIDTH = \&vwidth;
+
+sub sprintf {
+    my($format, @args) = @_;
+    my $conceal = Text::Conceal->new(
+	except => $format,
+	test   => $IS_TARGET,
+	length => $VISUAL_WIDTH,
+	max    => int @args,
+	);
+    $conceal->encode(@args) if $conceal;
+    my $s = CORE::sprintf $format, @args;
+    $conceal->decode($s)    if $conceal;
+    $s;
+}
+
+sub printf {
+    my $fh = ref($_[0]) =~ /^(?:GLOB|IO::)/ ? shift : select;
+    $fh->print(&sprintf(@_));
+}
+
+sub IsWideSpacing {
+    return <<"END";
++utf8::East_Asian_Width=Wide
++utf8::East_Asian_Width=FullWidth
+-utf8::Nonspacing_Mark
+END
+}
+
+sub vwidth {
+    local $_ = shift;
+    my $w;
+    while (/\G(?:(?<zero>\p{Nonspacing_Mark})|(?<two>\p{IsWideSpacing})|\X)/g) {
+	$w += $+{zero} ? 0 : $+{two} ? 2 : 1;
+    }
+    $w;
+}
+
+1;
+
+__END__
