7a1223dd96642ac392780202803ef9057ff99109
Kazumasa Utashiro <kaz@utashiro.com>
2022-08-09 12:15:24 +0900

    Checking in changes prior to tagging of version 1.01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fde5dcc..86d4a04 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Text-Conceal
     
     {{$NEXT}}
     
    +1.01 2022-08-09T03:15:17Z
    +
    +    - Release 1.01
    +
     0.9901 2021-08-30T03:08:19Z
     
         - Add test code.

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Text::Conceal - conceal and recover interface for text processing
 
 # VERSION
 
-Version 0.9901
+Version 1.01
 
 # DESCRIPTION
 
--- lib/Text/Conceal.pm
+++ lib/Text/Conceal.pm
@@ -1,6 +1,6 @@
 package Text::Conceal;
 
-our $VERSION = '0.9901';
+our $VERSION = '1.01';
 
 use v5.14;
 use warnings;
@@ -181,7 +181,7 @@ Text::Conceal - conceal and recover interface for text processing
 
 =head1 VERSION
 
-Version 0.9901
+Version 1.01
 
 =head1 DESCRIPTION
 
